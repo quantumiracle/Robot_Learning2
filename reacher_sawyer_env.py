af6722e13bab138ba77df84c274ba9082cb517a4
quantumiracle <1402434478@qq.com>
2019-07-23 22:34:30 +0100

    env

--- reacher_sawyer_env.py
+++ reacher_sawyer_env.py
@@ -16,6 +16,7 @@ from pyrep.robots.end_effectors.baxter_gripper import BaxterGripper
 from pyrep.objects.proximity_sensor import ProximitySensor
 from pyrep.objects.vision_sensor import VisionSensor
 from pyrep.objects.shape import Shape
+from pyrep.objects.dummy import Dummy
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -37,6 +38,7 @@ class ReacherEnv(object):
         self.agent.set_control_loop_enabled(False)
         self.agent.set_motor_locked_at_zero_velocity(True)
         self.target = Shape('target')  # object
+        self.end_point = Dummy('Sawyer_target')
         self.agent_ee_tip = self.agent.get_tip()
         self.initial_joint_positions = self.agent.get_joint_positions()
         self.action_space = np.zeros(7)  # 7 DOF velocity control
@@ -61,7 +63,7 @@ class ReacherEnv(object):
 
     def reset(self):
         # Get a random position within a cuboid and set the target position
-        pos = list(np.random.uniform(POS_MIN, POS_MAX))
+        pos = list(np.random.uniform(POS_MIN, POS_MAX, 3))
         self.target.set_position(pos)
         self.agent.set_joint_positions(self.initial_joint_positions)
         # set collidable, for collision detection
@@ -70,8 +72,13 @@ class ReacherEnv(object):
         return self._get_state()
 
     def step(self, action):
-        self.agent.set_joint_target_velocities(action)  # Execute action on arm
+        pos = list(np.random.uniform(POS_MIN, POS_MAX, 3))
+        self.end_point.set_position(pos)
+        # self.agent.set_joint_target_velocities(action)  # Execute action on arm
+        self.gripper.actuate(0, velocity=0.5)
         self.pr.step()  # Step the physics simulation
+        self.gripper.actuate(1, velocity=0.5)
+        self.pr.step() 
         # ax, ay, az = self.agent_ee_tip.get_position()
         ax, ay, az = self.gripper.get_position()
         tx, ty, tz = self.target.get_position()
@@ -82,7 +89,7 @@ class ReacherEnv(object):
         current_vision = self.vision_sensor.capture_rgb()  # capture a screenshot of the view with vision sensor
         plt.imshow(current_vision)
         plt.savefig('./img/vision.png')
-
+        
         # close the gripper if close enough to the object and the object is detected with the proximity sensor
         if distance<0.05 and self.proximity_sensor.is_detected(self.target)== True: 
             self.gripper.actuate(0, velocity=0.04)  # if done, close the hand, 0 for close and 1 for open.
@@ -99,3 +106,16 @@ class ReacherEnv(object):
         self.pr.stop()
         self.pr.shutdown()
 
+if __name__ == '__main__':
+    env=ReacherEnv(headless=False)
+    env.reset()
+    for step in range(1000):
+        print(step)
+        action=np.random.uniform(0,1, 7)  # action dim is 7
+        try:
+            env.step(action)
+        except KeyboardInterrupt:
+            print('Shut Down!')
+            env.shutdown()
+
+    env.shutdown()
